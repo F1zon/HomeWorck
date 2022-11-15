918e20701c7300488e8c050ac49f9f77f1b140ee
Nikita Koroliov <nik_iv02@mail.ru>
2022-11-16 00:49:00 +0700

    Решена 13 задача 2 урока

--- Lesson_2/Task_13/Program.cs
+++ Lesson_2/Task_13/Program.cs
@@ -17,19 +17,42 @@ getThree(number);
 
 
 
-int input(int number) {
+int input() {
     int inp = Convert.ToInt32(Console.ReadLine());
     return inp;
 }
 
-int getThree(int number) {
-    int get = 0;
-
+void getThree(int number) {
+    int end = 0;
+    int del = 10;
+    int coll = 1;
+    int three = 0;
+    
     if (number > 99)  {
-        int end = 0;
-
         while (end != 1) {
+            if (number / del != 0) {
+                if (coll == 10) {
+                    number = number % 10;
+                } else {
+                    del = del * 10;
+                }
+                coll++;
+            } else {
+                if (coll == 3) {
+                    three = number % 10;
+                } else {
+                    if (coll == 4) three = (number / 10) % 10;
+                    if (coll == 5) three = (number / 100) % 10;
+                    if (coll == 6) three = (number / 1000) % 10;
+                    if (coll == 7) three = (number / 10000) % 10;
+                    if (coll == 8) three = (number / 100000) % 10;
+                    if (coll == 9) three = (number / 1000000) % 10;
+                    if (coll == 10) three = ((number / 10) / 1000000) % 10;
+                }
 
+                end++;
+                Console.WriteLine($"three number = {three}");
+            }
         }
     } else {
         Console.WriteLine("третьей цифры нет");
