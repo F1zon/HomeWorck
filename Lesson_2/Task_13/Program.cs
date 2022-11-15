9f22d026561cae4c049b8b10c30dd6584f76554b
Nikita Koroliov <nik_iv02@mail.ru>
2022-11-16 01:20:58 +0700

    Оптимизация 13 задачи 2 урока

--- Lesson_2/Task_13/Program.cs
+++ Lesson_2/Task_13/Program.cs
@@ -23,36 +23,46 @@ int input() {
 }
 
 void getThree(int number) {
-    int end = 0;
     int del = 10;
     int coll = 1;
     
     if (number > 99)  {
-        while (end != 1) {
-            if (number / del != 0) {
-                if (coll == 10) {
-                    number = number % 10;
-                } else {
-                    del = del * 10;
-                }
-                coll++;
-            } else {
-                end++;
-            }
-        }
+        coll = GetColl(del, coll);
 
         int[] array = new int[coll];
-        del = 10;
 
-        for (int i = 0; i < coll; i++) {
-            array[i] = number % 10;
-            if (number / 10 != 0) del = del * 10;
-            number = number / 10;
-        }
-
-        Console.WriteLine($"Третье число = {array[coll - 3]}");
+        Console.WriteLine($"Третье число = {AddArray(del, coll, array)}");
 
     } else {
         Console.WriteLine("третьей цифры нет");
     }
+}
+
+int GetColl(int del, int coll) {
+    bool end = true;
+    while (end) {
+        if (number / del != 0) {
+            if (coll == 10) {
+                number = number % 10;
+            } else {
+                del = del * 10;
+            }
+            coll++;
+        } else {
+            end = false;
+        }
+    }
+
+    return coll;
+}
+
+int AddArray(int del, int coll, int[] array) {
+    del = 10;
+
+    for (int i = 0; i < coll; i++) {
+        array[i] = number % 10;
+        if (number / 10 != 0) del = del * 10;
+        number = number / 10;
+    }
+    return array[coll - 3];
 }
