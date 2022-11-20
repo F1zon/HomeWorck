cbcb1410d9306636857c9f646eac30544acc2567
Nikita Koroliov <nik_iv02@mail.ru>
2022-11-20 23:24:11 +0700

    Добавление 19 задания 2 урока

---
+++ Lesson_3/Task_19/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+        Задача 19
+
+        Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+        Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+        14212 -> нет
+
+        12821 -> да
+
+        23432 -> да
+*/
+
+
+
+
+// Ввод данных
+Console.WriteLine("Введите число:>> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Проверка числа
+if (number < 1) {
+    Console.WriteLine("Отрицательные числа не могут быть полиндромными!");
+    Environment.Exit(0);
+}
+
+// Проверка и вывод ответа на проверку полиндрома
+Console.WriteLine(checkPolinom(number));
+
+
+
+
+
+
+
+
+// Функции
+// Проверка на полином
+string checkPolinom(int num) {
+    int copyNum = num; // Копия числа "num" для создания перевёртыша
+    int shifter = 0;
+    int n = 1;
+
+    while (copyNum / 10 != 0) { // Находим размер числа
+        copyNum = copyNum / 10;
+        n++;
+    }
+    copyNum = num;
+
+    for (int i = 0; i < n; i++) { // Создаём перевёрнутую переменную
+        shifter = shifter * 10 + copyNum % 10;
+        copyNum = copyNum / 10;
+    }
+
+    return shifter == num
+        ? "Да" : "Нет";
+}
